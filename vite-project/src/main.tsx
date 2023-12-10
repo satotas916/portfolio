4db6f384ffa1e3e1fd1d5cb372903e6fc68fdb06
ediplex_sato <satotas916@gmail.com>
2023-12-10 17:40:51 +0900

    #2 component title

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './assets/scss/common.scss'
+import Title from './components/title.tsx'
+
+const title = { title:'Project', icon: 'terminal' }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Title {...title} />
   </React.StrictMode>,
 )
