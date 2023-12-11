e077df951ac7dd333a87fe15352d752536723a2d
ediplex_sato <satotas916@gmail.com>
2023-12-12 00:52:00 +0900

    [#4]btn

---
+++ vite-project/src/components/btn.tsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+function Btn(props: { text: string; url: string; }) {
+
+  // style
+  const Btn = styled.a`
+    position: relative;
+    display: inline-block;
+    padding: 0 10px;
+    font-size: 32px;
+    line-height: 1.44;
+    color: #000;
+    font-weight: bold;
+    font-family: 'Noto Sans JP', sans-serif;
+    text-decoration: none;
+    &::before {
+      content: '';
+      display: block;
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      height: 16px;
+      background: #4dac6e;
+      z-index: -1;
+      transform: scale(0, 1);
+      transform-origin: right top;
+      transition: transform 0.6s cubic-bezier(0.8, 0, 0.2, 1);
+    }
+    &:hover {
+      &:before {
+        transform:scale(1, 1);
+        transform-origin: left top;
+      }
+    }
+  `
+
+  return (
+    <Btn href={props.url}>
+      {props.text}
+    </Btn>
+  )
+}
+
+export default Btn
--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/scss/common.scss'
 import Title from './components/title.tsx'
+import Btn from './components/btn.tsx'
 
 const title = { title:'Project', icon: 'terminal' }
+const moreBtn = { text:'一覧を見る', url: '#' }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Title {...title} />
+    <Btn {...moreBtn} />
   </React.StrictMode>,
 )
