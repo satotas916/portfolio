5269399bceffcb63156f184efb0cc1a318a1bd20
ediplex_sato <satotas916@gmail.com>
2024-01-04 19:39:07 +0900

    #3 footer

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/scss/common.scss'
-import Title from './components/title/title.tsx'
+import Title from './components/title/Title.tsx'
 import Button from './components/button/Button.tsx'
+import Footer from './components/footer/Footer.tsx'
 
 const title = { title:'Project', icon: 'terminal' }
 const moreBtn = { text:'一覧を見る', url: '#' }
@@ -11,5 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Title {...title} />
     <Button {...moreBtn} />
+    <Footer />
   </React.StrictMode>,
 )
