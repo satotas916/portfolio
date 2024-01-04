9f686f8844e6c8bcf7077b4826fed8abb7056408
ediplex_sato <satotas916@gmail.com>
2024-01-04 19:43:50 +0900

    ディレクトリ移動

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './assets/scss/common.scss'
 import Title from './components/title/Title.tsx'
 import Button from './components/button/Button.tsx'
-import Footer from './components/footer/Footer.tsx'
+import Footer from './project/footer/Footer.tsx'
 
 const title = { title:'Project', icon: 'terminal' }
 const moreBtn = { text:'一覧を見る', url: '#' }
