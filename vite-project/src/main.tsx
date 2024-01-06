0071d2c7cd46d5f2451be4954511aa8bda8a7aa5
ediplex_sato <satotas916@gmail.com>
2024-01-07 01:54:03 +0900

    [features]skill #6

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/scss/common.scss'
 import Footer from './features/footer/Footer.tsx'
 import AboutContact from './features/about-contact/AboutContact.tsx'
 import AboutBlog from './features/about-blog/AboutBlog.tsx'
 import AboutProject from './features/about-project/AboutProject.tsx'
+import AboutSkill from './features/about-skill/AboutSkill.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
+    <AboutSkill />
     <AboutProject />
     <AboutBlog />
     <AboutContact />
