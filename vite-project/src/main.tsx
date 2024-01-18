0bd83779b8ce6b0a9d05eb9dbcc6926392f347bc
ediplex_sato <satotas916@gmail.com>
2024-01-19 02:45:03 +0900

    [features]kv #5

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,13 +1,15 @@
 import ReactDOM from 'react-dom/client'
 import './assets/scss/common.scss'
-import Footer from './features/footer/Footer.tsx'
-import AboutContact from './features/about-contact/AboutContact.tsx'
-import AboutBlog from './features/about-blog/AboutBlog.tsx'
-import AboutProject from './features/about-project/AboutProject.tsx'
+import Kv from './features/kv/Kv.tsx'
 import AboutSkill from './features/about-skill/AboutSkill.tsx'
+import AboutProject from './features/about-project/AboutProject.tsx'
+import AboutBlog from './features/about-blog/AboutBlog.tsx'
+import AboutContact from './features/about-contact/AboutContact.tsx'
+import Footer from './features/footer/Footer.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
+    <Kv />
     <AboutSkill />
     <AboutProject />
     <AboutBlog />
