9504c5bb9720fc8c0723c4aa344e3ece3da2a3ee
ediplex_sato <satotas916@gmail.com>
2024-02-05 00:12:03 +0900

    Top.tsx→page.tsxに踏襲

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,11 +1,58 @@
 'use client'
+import styled from 'styled-components'
 import '@/app/assets/scss/common.scss'
-import Top from '@/app/pages/Top';
+import Kv from '@/app/features/Kv/Kv'
+import AboutSkill from '@/app/features/AboutSkill/AboutSkill'
+import AboutProject from '@/app/features/AboutProject/AboutProject'
+import AboutBlog from '@/app/features/AboutBlog/AboutBlog'
+import AboutContact from '@/app/features/AboutContact/AboutContact'
+import Footer from '@/app/features/Footer/Footer'
+
+// style
+const Container = styled.div`
+  width: 980px;
+  margin: 0 auto;
+`
+const KvWrap = styled.div`
+  padding: 96px 0 0;
+`
+const Wrap = styled.div`
+  margin: 90px 0 0;
+`
+const SkillWrap = styled(Wrap)``
+const ProjectWrap = styled(Wrap)``
+const BlogWrap = styled(Wrap)``
+
+const ContactWrap = styled.div`
+  margin: 100px 0 0;
+`
+const FooterWrap = styled.div`
+  padding: 100px 0 0;
+`
 
 export default function Home() {
   return (
     <>
-      <Top />
+      <Container>
+        <KvWrap>
+          <Kv />
+        </KvWrap>
+        <SkillWrap>
+          <AboutSkill />
+        </SkillWrap>
+        <ProjectWrap>
+          <AboutProject />
+        </ProjectWrap>
+        <BlogWrap>
+          <AboutBlog />
+        </BlogWrap>
+        <ContactWrap>
+          <AboutContact />
+        </ContactWrap>
+      </Container>
+      <FooterWrap>
+        <Footer />
+      </FooterWrap>
     </>
   );
 }
